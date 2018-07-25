1d983aa29e2633c0ffef75b3783a5110b1c907cf
ke6ch <19749261+ke6ch@users.noreply.github.com>
2018-07-25 21:50:42 +0900

    新規作成

---
+++ src/main/java/nickel/moonstone/MoonstoneApplication.java
@@ -0,0 +1,12 @@
+package nickel.moonstone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonstoneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoonstoneApplication.class, args);
+	}
+}
---
+++ src/test/java/nickel/moonstone/MoonstoneApplicationTests.java
@@ -0,0 +1,16 @@
+package nickel.moonstone;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MoonstoneApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
